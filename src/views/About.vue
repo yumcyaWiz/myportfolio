b4c7449b0bc0efcac97e7f8c21d14a3e8e470e65
yumcyawiz <hashdolf1923@gmail.com>
2019-06-20 04:59:21 +0900

    write about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,13 @@
 <template>
-  <v-container>
-    <h1>About</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque dolor sit amet efficitur venenatis. In luctus id risus in cursus. Donec semper enim diam, non lobortis ex finibus et. Duis sit amet vehicula ex. Curabitur cursus malesuada lacus, fermentum mattis velit condimentum eget. Cras egestas turpis at ullamcorper consectetur. Fusce ultrices malesuada massa, quis laoreet nisi.
+  <v-container fluid>
+    <h1 class="display-1 font-weight-light">About</h1>
 
-Suspendisse felis nulla, ullamcorper at euismod eu, euismod ut justo. Integer a augue id dolor pulvinar placerat. Nullam molestie magna tortor, a aliquet metus suscipit nec. Morbi ac risus sapien. Nam risus eros, vulputate nec odio maximus, accumsan posuere orci. Donec posuere convallis pretium. Integer ut velit tempus, porttitor est id, auctor odio. Donec viverra euismod tellus. Fusce ut neque vel arcu consequat interdum mollis at dolor. Mauris ut lectus sit amet lectus molestie molestie bibendum et turpis.
-
-Praesent a dapibus dui. Proin tempus ac arcu nec lacinia. Quisque pellentesque augue eu aliquam finibus. Quisque commodo ligula viverra quam condimentum, et ultricies mi commodo. Aenean finibus fringilla ex, et euismod mi facilisis et. Maecenas finibus iaculis justo, ac efficitur justo consectetur at. Integer fringilla eget nunc feugiat congue. Vivamus suscipit nisi tortor, eu pulvinar mi malesuada at.
-
-Proin vel posuere mi, sit amet tincidunt dolor. Maecenas tristique nec massa nec aliquam. Integer maximus fermentum tellus, eu luctus urna tincidunt ut. Sed vitae quam lorem. In et massa ac odio imperdiet cursus. Praesent bibendum nibh sit amet felis condimentum, ut pharetra eros aliquam. Quisque a enim in risus porttitor malesuada. Suspendisse dictum, metus sed viverra egestas, nisl sapien luctus risus, eget rhoncus urna ligula ut mauris. Quisque vitae dapibus ex. Integer congue arcu erat, a mollis arcu pellentesque in. Nullam leo tortor, elementum eu facilisis et, malesuada eget diam. Cras id tincidunt felis, sit amet mollis lacus.
-
-Suspendisse potenti. Suspendisse magna risus, elementum vel gravida in, bibendum at arcu. Donec auctor ultricies eros, eu tempor ipsum finibus et. Fusce in dolor non lorem consequat egestas ac sollicitudin velit. Donec feugiat convallis est nec dignissim. Aenean mattis erat quis mauris sollicitudin, in imperdiet enim lobortis. Integer auctor condimentum dictum.</p>
+    <v-layout align-center justify-center column>
+      <v-avatar size=200><v-img :src="require('../assets/avatar.jpg')" /></v-avatar>
+      <h3 class="display-3 font-weight-light">yumcyawiz</h3>
+      <p>Hi, I am yumcyawiz.</p>
+      <p>I am an undergraduate student of some research university in Japan.</p>
+      <p>I major in Applied Mathematics.</p>
+    </v-layout>
   </v-container>
 </template>
