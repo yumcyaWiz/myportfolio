39ff21b35e592346ec69c4ee6bfeb337c31d7fa6
yumcyawiz <hashdolf1923@gmail.com>
2019-06-20 04:02:58 +0900

    add cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <v-container fluid>
     <v-parallax :src="require('../assets/shinkan2.png')">
       <v-layout align-center column justify-center>
         <h1 class="display-2 font-weight-thin mb-3">yumcyawiz</h1>
@@ -7,7 +7,12 @@
       </v-layout>
     </v-parallax>
 
-    <Card v-for="card in cards" :key="card.title" :title="card.title" :desc="card.desc" />
+    <v-container>
+      <h2>I am interested in ...</h2>
+      <v-flex v-for="card in cards" :key="card.title">
+        <Card :title="card.title" :desc="card.desc" />
+      </v-flex>
+    </v-container>
   </v-container>
 </template>
 
@@ -24,17 +29,25 @@ export default {
     return {
       cards: [
         { title: 'Mathematics',
-          desc: 'I am interested in various field of Mathematics, such as Statistics, Probability, Functional Analysis, Numerical Analysis, Stochastic Differential Equation, Control theory, and Stochastic Control.',
+          desc: 'I am interested in various field of Mathematics, such as Statistics, Computational Statistics, Probability, Real Analysis, Functional Analysis, Manifold, Numerical Analysis, Stochastic Differential Equation, Control theory, and Stochastic Control.',
           image: 'cards/mathematics.png'
+        },
+        {
+          title: 'Programming',
+          desc: 'I like doing programming and making a cool stuff with it. Personally I like to use programming on some mathematical projects such as numerical analysis, machine learning, simulation.'
         },
         { title: 'RayTracing',
           desc: 'RayTracing is a method to generate photorealistic CGs. I am especially interested in Montecarlo RayTracing algorithms such as Path Tracing, Bidirectional Path Tracing, Metropolis Light Transport.',
           image: 'cards/raytracing.png'
         },
         {
+          title: 'Mathematical Finance',
+          desc: 'I like how probability and stochastic differential equation theories are applied in option pricing.',
+        },
+        {
           title: 'Quantitative Investment',
           desc: 'Quantitative Investment is a way to compose a financial portfolio with mathematics. I am interested in how to make a low risk portfolio with mathematics.'
-        }
+        },
       ]
     }
   }
