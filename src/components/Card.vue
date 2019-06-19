afffb4be1fee033a4915d0624fe8ee8da1b7b475
yumcyawiz <hashdolf1923@gmail.com>
2019-06-20 03:34:02 +0900

    add cards

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-card-title primary-title>
+          <div>
+            <h3 class="heading mb-0">{{title}}</h3>
+            <div>{{desc}}</div>
+          </div>
+        </v-card-title>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+
+<script>
+export default {
+  props: {
+    title: String,
+    desc: String,
+    image: String
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,42 @@
 <template>
   <v-container>
-    <h1>Home</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque dolor sit amet efficitur venenatis. In luctus id risus in cursus. Donec semper enim diam, non lobortis ex finibus et. Duis sit amet vehicula ex. Curabitur cursus malesuada lacus, fermentum mattis velit condimentum eget. Cras egestas turpis at ullamcorper consectetur. Fusce ultrices malesuada massa, quis laoreet nisi.
+    <v-parallax :src="require('../assets/shinkan2.png')">
+      <v-layout align-center column justify-center>
+        <h1 class="display-2 font-weight-thin mb-3">yumcyawiz</h1>
+        <h4 class="subheading">a raytracing guy</h4>
+      </v-layout>
+    </v-parallax>
 
-Suspendisse felis nulla, ullamcorper at euismod eu, euismod ut justo. Integer a augue id dolor pulvinar placerat. Nullam molestie magna tortor, a aliquet metus suscipit nec. Morbi ac risus sapien. Nam risus eros, vulputate nec odio maximus, accumsan posuere orci. Donec posuere convallis pretium. Integer ut velit tempus, porttitor est id, auctor odio. Donec viverra euismod tellus. Fusce ut neque vel arcu consequat interdum mollis at dolor. Mauris ut lectus sit amet lectus molestie molestie bibendum et turpis.
-
-Praesent a dapibus dui. Proin tempus ac arcu nec lacinia. Quisque pellentesque augue eu aliquam finibus. Quisque commodo ligula viverra quam condimentum, et ultricies mi commodo. Aenean finibus fringilla ex, et euismod mi facilisis et. Maecenas finibus iaculis justo, ac efficitur justo consectetur at. Integer fringilla eget nunc feugiat congue. Vivamus suscipit nisi tortor, eu pulvinar mi malesuada at.
-
-Proin vel posuere mi, sit amet tincidunt dolor. Maecenas tristique nec massa nec aliquam. Integer maximus fermentum tellus, eu luctus urna tincidunt ut. Sed vitae quam lorem. In et massa ac odio imperdiet cursus. Praesent bibendum nibh sit amet felis condimentum, ut pharetra eros aliquam. Quisque a enim in risus porttitor malesuada. Suspendisse dictum, metus sed viverra egestas, nisl sapien luctus risus, eget rhoncus urna ligula ut mauris. Quisque vitae dapibus ex. Integer congue arcu erat, a mollis arcu pellentesque in. Nullam leo tortor, elementum eu facilisis et, malesuada eget diam. Cras id tincidunt felis, sit amet mollis lacus.
-
-Suspendisse potenti. Suspendisse magna risus, elementum vel gravida in, bibendum at arcu. Donec auctor ultricies eros, eu tempor ipsum finibus et. Fusce in dolor non lorem consequat egestas ac sollicitudin velit. Donec feugiat convallis est nec dignissim. Aenean mattis erat quis mauris sollicitudin, in imperdiet enim lobortis. Integer auctor condimentum dictum.</p>
+    <Card v-for="card in cards" :key="card.title" :title="card.title" :desc="card.desc" />
   </v-container>
 </template>
+
+
+<script>
+import Card from '../components/Card.vue'
+
+export default {
+  components: {
+    Card
+  },
+
+  data() {
+    return {
+      cards: [
+        { title: 'Mathematics',
+          desc: 'I am interested in various field of Mathematics, such as Statistics, Probability, Functional Analysis, Numerical Analysis, Stochastic Differential Equation, Control theory, and Stochastic Control.',
+          image: 'cards/mathematics.png'
+        },
+        { title: 'RayTracing',
+          desc: 'RayTracing is a method to generate photorealistic CGs. I am especially interested in Montecarlo RayTracing algorithms such as Path Tracing, Bidirectional Path Tracing, Metropolis Light Transport.',
+          image: 'cards/raytracing.png'
+        },
+        {
+          title: 'Quantitative Investment',
+          desc: 'Quantitative Investment is a way to compose a financial portfolio with mathematics. I am interested in how to make a low risk portfolio with mathematics.'
+        }
+      ]
+    }
+  }
+}
+</script>
