e1c0584d9357e30bff7e0665aa8c53646c0b3ae6
yumcyawiz <hashdolf1923@gmail.com>
2019-06-20 15:37:08 +0900

    nice header

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,9 @@ export default {
         { title: 'Home', icon: 'dashboard', path: '/', external: false },
         { title: 'About', icon: 'account_circle', path: '/about', external: false },
         { title: 'Blog', icon: '', path: 'https://teastat.blogspot.com/', external: true },
-        { title: 'Twitter', icon: '', path: 'https://twitter.com/yumcyawiz', external: true }
+        { title: 'Twitter', icon: '', path: 'https://twitter.com/yumcyawiz', external: true },
+        { title: 'Github', icon: '', path: 'https://github.com/yumcyaWiz', external: true },
+        { title: 'Pixiv BOOTH', icon: '', path: 'https://yumcyawiz.booth.pm/', external: true }
       ],
     }
   }
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-toolbar app>
+  <v-toolbar app dark>
     <v-toolbar-title>
       <v-avatar><v-img :src="require('../assets/avatar.jpg')" /></v-avatar>
       yumcyawiz
     </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items v-for="view in views" :key="view.title">
-      <v-btn flat :href="view.path" v-if="view.external">{{view.title}}</v-btn>
-      <v-btn flat :to="view.path" v-else>{{view.title}}</v-btn>
+      <v-btn flat small :href="view.path" v-if="view.external">{{view.title}}</v-btn>
+      <v-btn flat small :to="view.path" v-else>{{view.title}}</v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
