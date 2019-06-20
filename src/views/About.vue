50e40d40589d932aec330de32fd169c7082b1908
yumcyawiz <hashdolf1923@gmail.com>
2019-06-20 13:19:00 +0900

    relayout about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container fluid>
     <v-layout align-center justify-center column>
-      <v-avatar size=200><v-img :src="require('../assets/avatar.jpg')" /></v-avatar>
-      <h3 class="display-3 font-weight-light">yumcyawiz</h3>
+      <v-avatar size=100><v-img :src="require('../assets/avatar.jpg')" /></v-avatar>
+      <h3 class="display-3 font-weight-light mb-3">yumcyawiz</h3>
       <p>Hi, I am yumcyawiz.</p>
       <p>I am an undergraduate student of some research university in Japan.</p>
       <p>I major in Applied Mathematics.</p>
