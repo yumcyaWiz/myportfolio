917aad30a421a5e49df63a2ce238aa10f4ea3475
yumcyawiz <hashdolf1923@gmail.com>
2019-06-21 00:46:45 +0900

    add timeline on about

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,6 +8,34 @@
       <p>where I major in Applied Mathematics.</p>
       <p>I like to use programming on some mathematical projects.</p>
       <p>If you want to contact me, you can use DM on twitter or send me an e-mail to <code>hashdolf1923@gmail.com</code></p>
+
+      <v-timeline class="ma-3">
+        <v-timeline-item v-for="timeline in timelines" :key="timeline.time" color="red lighten-3">
+          <template v-slot:opposite>
+            <span>{{timeline.time}}</span>
+          </template>
+          <v-card class="elevation-2">
+            <v-card-title class="headline font-weight-light">{{timeline.title}}</v-card-title>
+            <v-card-text>{{timeline.desc}}</v-card-text>
+          </v-card>
+        </v-timeline-item>
+      </v-timeline>
     </v-layout>
   </v-container>
 </template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      timelines: [
+        { time: '2016/03', title: 'Enrolled in Undergraduate', desc: 'I enrolled in some research university in japan. I choosed to major in Applied Mathematics.' },
+        { time: '2016/12', title: 'Joined OSK', desc: 'OSK is a programming club in the university. I met a lot of cool guys there.' },
+        { time: '2017/08', title: 'Joined C92', desc: 'I published a book called Signed Distance Field. It is about introduction of RayMarching with GLSL.' },
+        { time: '2018/12', title: 'Joined C95', desc: 'I published a book called Photorealism. It is about theory and implementation of Path Tracing.' }
+      ]
+    }
+  }
+}
+</script>
