ffccc5358ff900f9a5aa5d1b43af9853e7d9f287
yumcyawiz <hashdolf1923@gmail.com>
2019-06-20 15:25:18 +0900

    rewrite about

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,8 +4,10 @@
       <v-avatar size=100><v-img :src="require('../assets/avatar.jpg')" /></v-avatar>
       <h3 class="display-3 font-weight-light mb-3">yumcyawiz</h3>
       <p>Hi, I am yumcyawiz.</p>
-      <p>I am an undergraduate student of some research university in Japan.</p>
-      <p>I major in Applied Mathematics.</p>
+      <p>I am an undergraduate student of some research university in Japan,</p>
+      <p>where I major in Applied Mathematics.</p>
+      <p>I like to use programming on some mathematical projects.</p>
+      <p>If you want to contact me, you can use DM on twitter or send me an e-mail to <code>hashdolf1923@gmail.com</code></p>
     </v-layout>
   </v-container>
 </template>
