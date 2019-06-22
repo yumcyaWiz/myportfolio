54cadd08a4df3d017db6fb5f342a1f6348f2acc3
yumcyawiz <hashdolf1923@gmail.com>
2019-06-22 14:46:24 +0900

    header mobile fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,11 +4,24 @@
       <v-avatar><v-img :src="require('../assets/avatar.jpg')" /></v-avatar>
       yumcyawiz
     </v-toolbar-title>
+
     <v-spacer></v-spacer>
-    <v-toolbar-items v-for="view in views" :key="view.title">
+
+    <v-toolbar-items v-for="view in views" :key="view.title" class="hidden-sm-and-down">
       <v-btn flat small :href="view.path" v-if="view.external">{{view.title}}</v-btn>
       <v-btn flat small :to="view.path" v-else>{{view.title}}</v-btn>
     </v-toolbar-items>
+
+    <v-menu class="hidden-md-and-up" dark>
+      <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+      <v-list>
+        <v-list-tile v-for="view in views" :key="view.title">
+          <v-list-tile-content>
+            <v-list-tile-title>{{view.title}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
   </v-toolbar>
 </template>
 
